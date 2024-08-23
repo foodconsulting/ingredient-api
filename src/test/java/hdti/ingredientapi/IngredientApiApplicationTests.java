27fe8c5550a82d162cc33847f20354f8ab9b8dd8
hereishd <hereishd>
2024-08-23 10:09:29 -0400

    initial commit

---
+++ src/test/java/hdti/ingredientapi/IngredientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hdti.ingredientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
